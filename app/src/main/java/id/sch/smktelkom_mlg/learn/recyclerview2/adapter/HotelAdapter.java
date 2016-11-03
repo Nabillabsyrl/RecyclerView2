ab71c2d047bc0af47c97ac6089fd1a556611215a
Nabila Basharahil <nabillabasharahil@gmail.com>
2016-11-03 07:07:22 -0700

    RecyclerView Grid

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/adapter/HotelAdapter.java
@@ -13,7 +13,8 @@ import id.sch.smktelkom_mlg.learn.recyclerview2.R;
 import id.sch.smktelkom_mlg.learn.recyclerview2.model.Hotel;
 
 
-public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> {
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
+{
     ArrayList<Hotel> hotelList;
 
     public HotelAdapter(ArrayList<Hotel> hotelList) {
@@ -32,7 +33,6 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
     public void onBindViewHolder(ViewHolder holder, int position) {
         Hotel hotel = hotelList.get(position);
         holder.tvJudul.setText(hotel.judul);
-        holder.tvDeskripsi.setText(hotel.deskripsi);
         holder.ivFoto.setImageDrawable(hotel.foto);
 
     }
@@ -47,14 +47,12 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView ivFoto;
         TextView tvJudul;
-        TextView tvDeskripsi;
 
 
         public ViewHolder(View itemView) {
             super(itemView);
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
-            tvJudul = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
-            tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+            tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
 
         }
     }
